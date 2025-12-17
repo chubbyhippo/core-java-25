b43368a367a1e6678b41eb55a6db185ad1506bea
chubbyhippo <mkmsacc@outlook.com>
2025-12-17 21:15:04 +0700

    Add `DrawComponent` stub with rectangle and ellipse drawing setup

--- src/main/java/io/github/chubbyhippo/v2ch10/draw/DrawDemo.java
+++ src/main/java/io/github/chubbyhippo/v2ch10/draw/DrawDemo.java
@@ -6,5 +6,26 @@ class DrawDemo {
 
 }
 
+/**
+ * A component that displays rectangles and ellipses
+ */
+
+class DrawComponent extends JComponent {
+    private static final int DEFAULT_WIDTH = 400;
+    private static final int DEFAULT_HEIGHT = 400;
+
+    public void paintComponent(Graphics g) {
+        var g2 = (Graphics2D) g;
+
+        // draw a rectangle
+        double leftX = 100;
+        double topY = 100;
+        double width = 200;
+        double height = 150;
+
+    }
+
+}
+
 
 
